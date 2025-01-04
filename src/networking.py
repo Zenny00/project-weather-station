2645daad0fc289b17ca896ac52cfbaba74766140
Zenny00 <joshuacomfort@gmail.com>
2025-01-04 15:46:03 -0500

    Add ERD

--- src/networking.py
+++ src/networking.py
@@ -2,11 +2,9 @@ import network
 from time import sleep
 
 def connect_to_network(ssid, password):
-    print(ssid, password)
     # Connect to the WiFi network
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    print(wlan.scan)
     wlan.connect(ssid, password)
     
     # Wait for Wi-Fi connection
@@ -20,7 +18,7 @@ def connect_to_network(ssid, password):
 
     # Check if connection is successful
     if wlan.status() != 3:
-        raise RuntimeError('Failed to establish a network connection')
+        print('Failed to establish a network connection')
     else:
         print('Connection successful!')
         network_info = wlan.ifconfig()
