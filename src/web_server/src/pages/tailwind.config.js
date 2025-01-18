66d36c9a0772e270b0303a43cf6e2b1051479025
Zenny00 <joshuacomfort@gmail.com>
2025-01-18 16:06:29 -0500

    Setup initial webpage and get tailwind working

---
+++ src/web_server/src/pages/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html, js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sagoe: ["Segoe UI", "sans-serif"],
+      oxygen: ["Oxygen", "sans-serif"],
+      ubuntu: ["Ubuntu", "sans-serif"]
+    }
+  },
+  plugins: [],
+}
+
