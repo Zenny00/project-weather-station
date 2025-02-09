023a4657ca7c07834d64fc680e1de94ca2cddbd0
Zenny00 <joshuacomfort@gmail.com>
2025-02-08 22:56:51 -0500

    Setup remaining units

--- src/web_server/src/pages/index.js
+++ src/web_server/src/pages/index.js
@@ -1,6 +1,10 @@
 // Overall options for the application
 const applicationState = {
   temperature_unit: "F",
+  pressure_unit: "mB",
+  humidity_unit: "%",
+  precipitation_unit: "in.",
+  wind_speed_unit: "m/s",
   get_state() {
     return { ...this };
   }
@@ -84,6 +88,7 @@ function display_conditions(measurement, componentId) {
   const state = applicationState.get_state();
 
   // Get values to be displayed from the measurement and format for display
+
   // Get the unit to determine how to display temperature
   let temperature;
   if (state.temperature_unit === "F") {
@@ -92,6 +97,30 @@ function display_conditions(measurement, componentId) {
     temperature = measurement.temperature;
   }
   temperature = temperature.toFixed(1) + " °" + state.temperature_unit;
+
+  // Get the unit to determine how to display humidity
+  let humidity;
+  if (state.humidity_unit === "%") {
+    humidity = measurement.humidity;
+  }
+  humidity = humidity.toFixed(1) + state.humidity_unit;
+  
+  // Get the unit to determine how to display wind speed
+  let wind_speed;
+  if (state.wind_speed_unit === "m/s") {
+    wind_speed = measurement.wind_speed;
+  }
+  wind_speed = wind_speed.toFixed(1) + state.wind_speed_unit;
+  
+  // Get the unit to determine how to display precipitation
+  let precipitation;
+  if (state.precipitation_unit === "in.") {
+    precipitation = measurement.precipitation / 2.54;
+  } else {
+    precipitation = measurement.precipitation;
+  }
+  precipitation = precipitation.toFixed(1) + state.precipitation_unit;
+  
 
   let conditions_text = document.getElementById(componentId + "_text");
   conditions_text.innerText = temperature;
